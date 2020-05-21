9b76e7114c8fab30337ab8445015281a44bfb05e
Uriah Harston <uharston@gmail.com>
2020-05-21 19:24:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,16 +9,16 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |p|
     projects[p] = {}
-  end 
+  end
 
-  projects 
+  projects
 end
 #projects: kickstarter.css("li.project.grid_4")
-#title: project.css("h2.bbcard_name strong a").text 
+#title: project.css("h2.bbcard_name strong a").text
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
-#description: project.css("p.bbcard_blurb").text 
-#location: project.css("span.location-name").text 
-#percent_funded: project.css("ul.project-stats li.first.funded strong").text 
+#description: project.css("p.bbcard_blurb").text
+#location: project.css("span.location-name").text
+#percent_funded: project.css("ul.project-stats li.first.funded strong").text
 
 
 
