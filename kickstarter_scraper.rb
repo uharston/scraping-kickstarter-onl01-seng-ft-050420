aed324d61747d1fa7add7ceb2f72779af8b8ebf9
Uriah Harston <uharston@gmail.com>
2020-05-21 19:01:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ def create_project_hash
   html = File.read("fixtures/kickstarter.html")
 
   kickstarter = Nokogiri::HTML(html)
-end
 binding.pry
+  #kickstarter.css("li.project grid_4").first 
+end
+
 create_project_hash
